62c136f5c7012e4a4da50870342321fd42149f24
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-15 14:05:15 +0900

    [Feature Implementation] Refurbish the FRDialogFragment project #9
    * Add sample

--- sample/src/main/java/org/fog_rock/frdialogfragmentsample/MainActivity.kt
+++ sample/src/main/java/org/fog_rock/frdialogfragmentsample/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.fog_rock.frdialogfragmentsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.fog_rock.frdialogfragment.FRAppCompatActivity
+import org.fog_rock.frdialogfragment.FRDialogFragment
+import org.fog_rock.frdialogfragmentsample.databinding.ActivityMainBinding
+import org.fog_rock.frextensions.androidx.log.logI
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FRAppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val callbackKey = registerForDialogResult {
+        logI("callback result: $it")
+        binding.textViewResult.text = "Callback Result: $it"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonDialog.setOnClickListener {
+            FRDialogFragment.Builder(this).apply {
+                setTitle(R.string.title)
+                setMessage(R.string.message)
+                setPositiveButton(R.string.ok)
+                setNegativeButton(R.string.cancel)
+                setCancelable(true)
+                setCallbackKey(callbackKey)
+            }.create().show(supportFragmentManager, null)
+        }
     }
 }
