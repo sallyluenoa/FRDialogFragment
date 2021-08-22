c81bda8ebb59cacfb978df6b93890d74e3a30bb5
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-23 00:27:01 +0900

    Add sample and test code #5
    * Modify MainActivity

--- sample/src/main/java/org/fog_rock/frfragmentlistenersample/MainActivity.kt
+++ sample/src/main/java/org/fog_rock/frfragmentlistenersample/MainActivity.kt
@@ -1,19 +1,13 @@
 package org.fog_rock.frfragmentlistenersample
 
 import android.os.Bundle
-import org.fog_rock.frextensions.androidx.log.logI
-import org.fog_rock.frfragmentlistener.activity.FRAppCompatActivity
-import org.fog_rock.frfragmentlistener.dialog.FRDialogFragment
+import androidx.appcompat.app.AppCompatActivity
+import org.fog_rock.frextensions.androidx.content.startActivity
 import org.fog_rock.frfragmentlistenersample.databinding.ActivityMainBinding
 
-class MainActivity : FRAppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private val callbackKey = registerForDialogResult {
-        logI("callback result: $it")
-        binding.textViewResult.text = "Callback Result: $it"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +15,11 @@ class MainActivity : FRAppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonDialog.setOnClickListener {
-            FRDialogFragment.Builder(this).apply {
-                setTitle(R.string.title)
-                setMessage(R.string.message)
-                setPositiveButton(R.string.ok)
-                setNegativeButton(R.string.cancel)
-                setCancelable(true)
-                setCallbackKey(callbackKey)
-            }.create().show(supportFragmentManager, null)
+        binding.buttonDialogSample.setOnClickListener {
+            startActivity<DialogSampleActivity>()
+        }
+        binding.buttonFragmentSample.setOnClickListener {
+            startActivity<FragmentSampleActivity>()
         }
     }
 }
