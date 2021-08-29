11f1491f87a264ac109a8d909c1398a42548f74e
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-29 11:06:46 +0900

    Add sample and test code #5
    * Apply listener event to FragmentListenerTestActivity.

--- sample/src/main/java/org/fog_rock/frfragmentlistenersample/test/FragmentListenerTestActivity.kt
+++ sample/src/main/java/org/fog_rock/frfragmentlistenersample/test/FragmentListenerTestActivity.kt
@@ -1,7 +1,9 @@
 package org.fog_rock.frfragmentlistenersample.test
 
 import android.os.Bundle
+import org.fog_rock.frextensions.androidx.fragment.replaceFragment
 import org.fog_rock.frfragmentlistener.activity.FRAppCompatActivity
+import org.fog_rock.frfragmentlistenersample.R
 import org.fog_rock.frfragmentlistenersample.databinding.ActivityFragmentListenerTestBinding
 
 /**
@@ -11,10 +13,26 @@ class FragmentListenerTestActivity : FRAppCompatActivity() {
 
     private lateinit var binding: ActivityFragmentListenerTestBinding
 
+    private val fragmentListenerKey = registerForFragmentListener(
+        object : FragmentListenerTestFragment.Listener {
+            override fun onClickedButton() {
+                binding.textViewResult.text = "Button clicked."
+            }
+            override fun onSelectedSpinnerItem(item: String) {
+                binding.textViewResult.text = "Item selected: $item"
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityFragmentListenerTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        replaceFragment(
+            FragmentListenerTestFragment.newInstance(fragmentListenerKey),
+            R.id.fragment_container
+        )
     }
 }
