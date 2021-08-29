fda575c8fb4bbe5642646fd0cb98c2fb63b9945e
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-29 14:34:29 +0900

    Add sample and test code #5
    * Create classes for test codes.
    * Remove default test codes.

---
+++ sample/src/androidTest/java/org/fog_rock/frfragmentlistenersample/test/DialogFragmentTest.kt
@@ -0,0 +1,11 @@
+package org.fog_rock.frfragmentlistenersample.test
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+/**
+ * @see DialogFragmentTestActivity
+ */
+@RunWith(AndroidJUnit4::class)
+class DialogFragmentTest {
+}
---
+++ sample/src/androidTest/java/org/fog_rock/frfragmentlistenersample/test/FragmentListenerTest.kt
@@ -0,0 +1,11 @@
+package org.fog_rock.frfragmentlistenersample.test
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+/**
+ * @see FragmentListenerTestActivity
+ */
+@RunWith(AndroidJUnit4::class)
+class FragmentListenerTest {
+}
