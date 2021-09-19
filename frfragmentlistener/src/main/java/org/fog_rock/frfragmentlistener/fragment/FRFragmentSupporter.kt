3446563291ad8e626eed84b5d5fec8b1422a648d
SallyLueNoa <sally.lue.noa@gmail.com>
2021-09-19 23:12:48 +0900

    Support for Java #21
    * Add FRFragmentSupporter to call from Java source code.

---
+++ frfragmentlistener/src/main/java/org/fog_rock/frfragmentlistener/fragment/FRFragmentSupporter.kt
@@ -0,0 +1,28 @@
+package org.fog_rock.frfragmentlistener.fragment
+
+import androidx.fragment.app.Fragment
+import org.fog_rock.frextensions.androidx.log.logW
+import org.fog_rock.frfragmentlistener.activity.FRAppCompatActivity
+
+class FRFragmentSupporter<T : FRFragmentListener?> {
+
+    fun restoreFragmentEventListener(fragment: Fragment): T? {
+        val callbackKey = fragment.arguments?.getString(FRFragmentListener.ARGS_LISTENER_KEY) ?: run {
+            logW("Not found fragment listener key.")
+            return null
+        }
+        val activity = fragment.requireActivity() as? FRAppCompatActivity ?: run {
+            logW("Activity does not extends FRAppCompatActivity.")
+            return null
+        }
+        val listener = activity.fragmentListenerHolder[callbackKey] ?: run {
+            logW("Not found fragment listener from holders.")
+            return null
+        }
+        @Suppress("UNCHECKED_CAST")
+        return listener as? T ?: run {
+            logW("Invalid subclass of FragmentEventListener.")
+            return null
+        }
+    }
+}
